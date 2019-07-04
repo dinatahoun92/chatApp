08333fae86fde48bd0d20fddd63474ef7d7a6cc4
dinatahoun92 <dinatahoun992@gmail.com>
2019-07-04 16:44:02 +0200

    sign in

--- src/components/root.js
+++ src/components/root.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Route, Switch } from "react-router-dom";
 import App from "./App";
 import Registeration from "./registeration";
-import Login from "../login";
+import Login from "./login";
+import firebase from "../logic/firebase";
+import { useSelector, useDispatch } from "react-redux";
+import * as actionCreator from "../actions/actions";
 
-export default function Root() {
+export default function Root({ history }) {
+  const user = useSelector(state => state.user);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        console.log("allready logged in -> navigate to /");
+        dispatch(
+          actionCreator.user({
+            user: user.displayName,
+            userId: user.uid
+          })
+        );
+        history.push("/");
+      } else {
+        console.log("not logged in -> navigate to /Login");
+        history.push("/Login");
+      }
+    });
+  }, []);
   return (
     <Switch>
       <Route exact path="/" component={App} />
