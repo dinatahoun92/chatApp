770b3447caed236b9e71c3ab74504c923e9fcdc1
dinatahoun92 <dinatahoun992@gmail.com>
2019-07-09 01:42:37 +0200

    Updates

---
+++ precache-manifest.d84982e3b93f04c5abcb195441811ec3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "452999faa7b98ac1dffaefeeaa415dd6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5394b501e88ff53ec48",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "bdd2dbbf2ee86d6cc079",
+    "url": "/static/js/2.bf87ea8c.chunk.js"
+  },
+  {
+    "revision": "b5394b501e88ff53ec48",
+    "url": "/static/js/main.ae3d66b9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
